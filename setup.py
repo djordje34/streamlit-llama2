57067e95b9e8c44f0b6789bee6956bfdcaf71cbe
Djordje <83914973+djordje34@users.noreply.github.com>
2023-07-27 22:24:56 +0200

    Add files via upload
    ...............................

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ with st.sidebar:
     else:
         llm = 'replicate/llama70b-v2-chat:e951f18578850b652510200860fc4ea62b3b16fac280f83ff32282f87bbd2e48'
     
-    temperature = st.sidebar.slider('temperature', min_value=1.0, max_value=5.0, value=1, step=0.01)
+    temperature = st.sidebar.slider('temperature', min_value=0.01, max_value=5.0, value=0.1, step=0.01)
     top_p = st.sidebar.slider('top_p', min_value=0.01, max_value=1.0, value=0.9, step=0.01)
     max_length = st.sidebar.slider('max_length', min_value=64, max_value=4096, value=512, step=8)
 os.environ['REPLICATE_API_TOKEN'] = replicate_api
